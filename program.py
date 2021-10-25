92b514325fa932054da6206b1f8c17fb7cadd479
robertsomatic <robertsomatic@gmail.com>
2021-10-25 11:34:55 -0400

    Added inital code

--- program.py
+++ program.py
@@ -5,6 +5,40 @@ from datetime import datetime
 def main():
     print("Hello")
 
+    r = sr.Recognizer()
+    run = True
+    data = {}
+    
+    with open("notepad.json", "r") as f:
+        data = json.load(f)
+
+    while run: 
+        with sr.Microphone() as source:
+            print("Working")
+            audio_text = r.listen(source)
+            print("Done")
+
+            try:
+                text = r.recognize_google(audio_text)
+                print(json.dumps(data, indent = 4))
+
+                # create json file
+                now = datetime.now()
+                timestamp = datetime.timestamp(now)
+
+                data["notes"].append({
+                    str(timestamp) : text
+                    })
+
+                with open("notepad.json", "w") as outfile:
+                    outfile.write(json.dumps(data, indent = 4))
+
+            except KeyboardInterrupt:
+                print("Stopping")
+            except:
+                print("Error")
+                run = False
+
 if __name__ == '__main__':
     main()
 
